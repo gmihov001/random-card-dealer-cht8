230e3c616b8c1fbca492151e55ad534cb6626ca6
George Mihov <gmihov001@gmail.com>
2020-09-30 12:51:06 +0000

    Updates

--- src/js/index.js
+++ src/js/index.js
@@ -9,15 +9,20 @@ window.onload = function() {
   myFunction();
 };
 
-const myFunction = () => {};
-let suits = ["&#9824", "&#9827", "&#9829", "&#9830"];
-let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
-let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+const myFunction = () => {
+  let suits = ["&#9824", "&#9827", "&#9829", "&#9830"];
+  let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
 
-if (randomSuit === "&#9830" || randomSuit === "&#9829") {
-  document.querySelector(".card").style.color = "red";
-  // document.querySelector("#top").style.color = "red";
-  // document.querySelector("#middle").style.color = "red";
-  // document.querySelector("#bottom").style.color = "red";
-}
+  if (randomSuit === "&#9830" || randomSuit === "&#9829") {
+    document.querySelector(".card").style.color = "red";
+    // document.querySelector("#top").style.color = "red";
+    // document.querySelector("#middle").style.color = "red";
+    // document.querySelector("#bottom").style.color = "red";
+  }
+
+  document.querySelector(".top").innerHTML = randomSuit;
+  document.querySelector(".middle").innerHTML = randomNumber;
+  document.querySelector(".bottom").innerHTML = randomSuit;
+};
